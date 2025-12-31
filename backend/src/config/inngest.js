31bcd84c6fba637c6808f417d0d3c8247a5ac24e
Nischal <nyyscal541556@gmail.com>
2025-12-31 18:05:11 +0545

    Inngest Webhook Created

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -15,7 +15,7 @@ const syncUser = inngest.createFunction(
     const newUser = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      name: `${first_name} ${last_name}` || "User",
+      name: `${first_name || ""} ${last_name|| ""}` || "User",
       imageUrl: image_url || "",
       addresses: [],
       wishlist: [],
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,16 @@ import path from "path"
 import { ENV } from "./config/env.js"
 import { connectDB } from "./config/db.js"
 import { clerkMiddleware } from "@clerk/express";
-
+import {serve} from "inngest/express"
+import { inngest, functions } from "./config/inngest.js"
 const app = express()
 
 const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(clerkMiddleware()) //adds auth object under the request
+
+app.use("/api/inngest",serve({client:inngest, functions}))
 
 app.get("/api/health",(req,res)=>{
   res.status(200).send("Server is healthy.")
@@ -30,5 +33,5 @@ if(ENV.NODE_ENV==="production"){
 
 app.listen(ENV.PORT,async()=>{
   await connectDB()
-  console.log(`Server is running on port http://localhost:3000`)
+  console.log(`🚀 Server is running on port http://localhost:3000`)
 })

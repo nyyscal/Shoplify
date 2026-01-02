cc39d868fa766cdc3de4cf5175832f35ec2da09a
Nischal <nyyscal541556@gmail.com>
2026-01-02 22:28:49 +0545

    Server Changes Minor

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import { inngest, functions } from "./config/inngest.js"
 
 import adminRoutes from "./routes/admin.route.js"
 import userRoutes from "./routes/user.route.js"
+import orderRoutes from "./routes/order.route.js"
 
 
 const app = express()
@@ -20,6 +21,7 @@ app.use(clerkMiddleware()) //adds auth object under the request
 app.use("/api/inngest",serve({client:inngest, functions}))
 app.use("/api/admin",adminRoutes)
 app.use("/api/user",userRoutes)
+app.use("/api/orders",orderRoutes)
 
 app.get("/api/health",(req,res)=>{
   res.status(200).send("Server is healthy.")

2e0d2cc9ea8b0b809493d0d5576b172ffefa830b
Nischal <nyyscal541556@gmail.com>
2026-01-03 18:46:42 +0545

    Review and Product Routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,8 @@ import { inngest, functions } from "./config/inngest.js"
 import adminRoutes from "./routes/admin.route.js"
 import userRoutes from "./routes/user.route.js"
 import orderRoutes from "./routes/order.route.js"
+import reviewRoutes from "./routes/review.route.js"
+import productRoutes from "./routes/product.route.js"
 
 
 const app = express()
@@ -19,9 +21,12 @@ app.use(express.json())
 app.use(clerkMiddleware()) //adds auth object under the request
 
 app.use("/api/inngest",serve({client:inngest, functions}))
+
 app.use("/api/admin",adminRoutes)
 app.use("/api/user",userRoutes)
 app.use("/api/orders",orderRoutes)
+app.use("/api/reviews",reviewRoutes)
+app.use("/api/products",productRoutes)
 
 app.get("/api/health",(req,res)=>{
   res.status(200).send("Server is healthy.")

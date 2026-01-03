28bb2351e15102aecb241220d071cd1182a066d1
Nischal <nyyscal541556@gmail.com>
2026-01-03 18:05:03 +0545

    Order Controller

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -1,4 +1,6 @@
 import { Product } from "../models/product.model.js";
+import { Order } from "../models/order.model.js";
+import { Review } from "../models/review.model.js";
 
 export async function createOrder(req,res){
   try {
@@ -19,11 +21,43 @@ export async function createOrder(req,res){
         return res.status(400).json({message:`Insufficient stock for product ${product.name}`})
       }
     }
+    const order = await Order.create({
+      user: user._id,
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      totalPrice
+    })
+
+    //update prodcut stock -1 for ordered stock
+    for (const item of orderItems){
+      await Product.findByIdAndUpdate(item.product._id,{
+        $inc: {stock: -item.quantity}, //decrement by 1
+      })
+    }
+
+    res.status(201).json({message:"Order created successfully", order})
   } catch (error) {
      console.error("Error creating order:", error);
     res.status(500).json({message:"Internal server error"})
   }
 }
-export async function getUserOrder(req,res){
 
+export async function getUserOrder(req,res){
+  try {
+    const orders = (await Order.find({clerkId:req.user.clerkId}).populate('orderItems.product')).sort({createdAt:-1})
+
+    //check if each order has been reviewed
+    const orderWithReviewStatus = await Promise.all(orders.map(async (order)=>{
+      const review = await Review.findOne({orderId: order._id})
+      return {
+        ...order.toObject(),
+        hasReviewed: !!review, //double bang operatror to convert to boolean
+      }
+    }))
+    res.status(200).json({orders: orderWithReviewStatus})
+  } catch (error) {
+    console.error("Error getting order:", error);
+    res.status(500).json({message:"Internal server error"})
+  }
 }

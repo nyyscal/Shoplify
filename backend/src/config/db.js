b137ace345d4c6465ee33f6605d137a4ea0b1717
Nischal <nyyscal541556@gmail.com>
2026-01-09 23:29:03 +0545

    DB Configurations Added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,12 @@ import {ENV} from "./env.js"
 
 export const connectDB = async()=>{
   try {
-    const conn = await mongoose.connect(ENV.DB_URL)
+    const conn = await mongoose.connect(ENV.DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // 30 seconds
+    socketTimeoutMS: 30000,
+    })
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
     console.log("❌ Error in DB connection",error)

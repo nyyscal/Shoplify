65ab781c27523a89b487a635b421948581efd96f
Nischal <nyyscal541556@gmail.com>
2026-01-10 22:18:46 +0545

    Tanstack & Clerk Provider Integrated

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+import "../global.css"
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
-  return <Stack screenOptions={{headerShown:false}}/>;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{headerShown:false}}/>;
+    </QueryClientProvider>
+    </ClerkProvider>
+  )
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,9 +1,8 @@
 import { Text, View } from "react-native";
-import "../global.css"
 export default function Index() {
   return (
      <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
+      <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
     </View>

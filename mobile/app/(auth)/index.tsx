5ffe3eaff78ff95ad8ef29dd7ee971ee11bfde40
Nischal <nyyscal541556@gmail.com>
2026-01-11 22:40:27 +0545

    Clerk Integration in React Native

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,10 +1,63 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity, ActivityIndicator } from 'react-native'
 import React from 'react'
+import useSocialAuth from '@/hooks/useSocialAuth'
 
 const AuthScreen = () => {
+  const {isLoading, handleSocialAuth} =useSocialAuth()
   return (
-    <View>
-      <Text>AuthScreen</Text>
+    <View className='px-8 flex-1 justify-center items-center'>
+      {/* DEMO IMAGE */}
+      <Image source={require("../../assets/images/auth-image.png")} 
+      className='size-96' 
+      resizeMode='contain'
+      />
+      <View className='gap-2 mt-3'>
+        {/* Google Sign-In */}
+        <TouchableOpacity className='flex-row itemce] justify-center border border-gray-300 rounded-full px-6 py-3'
+        onPress={()=>handleSocialAuth("oauth_google")}
+        disabled={isLoading}
+        style={{
+          shadowOffset:{width:0, height:1},
+          shadowOpacity:0.1,
+          // elevation: 2
+    }}
+        >
+          {isLoading ? (
+            <ActivityIndicator size={"small"} color={"#4285f4"}/>
+          ):(
+            <View className='flex-row items-center justify-center'>
+              <Image source={require("../../assets/images/google.png")} 
+              className='size-10 mr-3' resizeMode='contain'/>
+              <Text className='text-black font-medium text-base'>Sign in with Google</Text>
+            </View>
+          )}
+        </TouchableOpacity>
+        {/* Apple Sign-In */}
+        <TouchableOpacity className='flex-row itemce] justify-center border border-gray-300 rounded-full px-6 py-3'
+        onPress={()=>handleSocialAuth("oauth_apple")}
+        disabled={isLoading}
+        style={{
+          shadowOffset:{width:0, height:1},
+          shadowOpacity:0.1,
+          // elevation: 2
+    }}
+        >
+          {isLoading ? (
+            <ActivityIndicator size={"small"} color={"#4285f4"}/>
+          ):(
+            <View className='flex-row items-center justify-center'>
+              <Image source={require("../../assets/images/apple.png")} 
+              className='size-8 mr-3' resizeMode='contain'/>
+              <Text className='text-black font-medium text-base'>Sign in with Apple</Text>
+            </View>
+          )}
+        </TouchableOpacity>
+      </View>
+
+      {/* Terms */}
+      <Text className='text-center text-gray-500 text-xs leading-4 mt-6 px-2'>
+        By signing in, you agree to our <Text className='text-blue-500'>Terms of Service</Text> and <Text className='text-blue-500'>Privacy Policy</Text>.
+      </Text>
     </View>
   )
 }
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,18 +1,23 @@
 import { Stack } from "expo-router";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import { ClerkProvider } from '@clerk/clerk-expo'
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
-import "../global.css"
+import "../global.css";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
-    <QueryClientProvider client={queryClient}>
-      <Stack screenOptions={{headerShown:false}}/>;
-    </QueryClientProvider>
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}
+    >
+      <ClerkLoaded>
+        <QueryClientProvider client={queryClient}>
+          <Stack screenOptions={{ headerShown: false }} />
+        </QueryClientProvider>
+      </ClerkLoaded>
     </ClerkProvider>
-  )
+  );
 }

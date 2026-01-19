61325a531dd1b293aea0f1b3c1d4ff8428a8c80f
Nischal <nyyscal541556@gmail.com>
2026-01-19 22:23:28 +0545

    Tab Layout Completed

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -31,7 +31,7 @@ const TabsLayout = () => {
           <BlurView
             intensity={80}
             tint="dark"
-            // style={StyleSheet.absoluteFill}
+            style={{ position: "absolute", top: 0, right: 0, left: 0, bottom: 0 }}
             // StyleSheet.absoluteFill is equal to this 👇
             // { position: "absolute", top: 0, right: 0, left: 0, bottom: 0 }
           />

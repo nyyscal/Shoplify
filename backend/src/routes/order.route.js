310acc5d18bea4bd5d98c1071861953b941175e2
Nischal <nyyscal541556@gmail.com>
2026-02-11 14:12:26 +0545

    Order Route Fix

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { createOrder, getUserOrder } from "../controllers/order.controller.js";
+import { createOrder, getUserOrders } from "../controllers/order.controller.js";
 
 const router = Router()
 
 router.post("/",protectRoute,createOrder)
-router.get("/",protectRoute,getUserOrder)
+router.get("/",protectRoute,getUserOrders)
 
 export default router
